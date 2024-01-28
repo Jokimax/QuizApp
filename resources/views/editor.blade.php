bd403d888ae3a9cc23f4b341fbd5b4326ebe5ca1
Jokimax <jonaxthedreadlord@gmail.com>
2024-01-28 19:26:04 +0200

    change page titles

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Quiz</title>
+    <title>Quiz Editor</title>
     @vite('resources/css/editor.css')
     @vite('resources/js/Pages/editor.jsx')
 </head>
--- resources/views/quiz.blade.php
+++ resources/views/quiz.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Create Quiz</title>
+    <title>Quiz</title>
     @vite('resources/css/quiz.css')
     @vite('resources/js/Pages/quiz.jsx')
 </head>
